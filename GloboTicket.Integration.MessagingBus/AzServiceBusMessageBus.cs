ba39bfd532ba68d8f3db6846fa8d0ff719711409
CipriRosca <cipri.rosca93@yahoo.com>
2022-03-01 16:35:49 +0200

    Initial commit

---
+++ GloboTicket.Integration.MessagingBus/AzServiceBusMessageBus.cs
@@ -0,0 +1,32 @@
+﻿using GloboTicket.Integration.Messages;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.Integration.MessagingBus
+{
+    public class AzServiceBusMessageBus: IMessageBus
+    {
+        private string connectionString = 
+            "Endpoint=sb://globalticket-sample.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=I6tojRVtNe2KYo0Ga5JPOdgUjzvnCs7O2/Sk7g/miF0=";
+
+        public async Task PublishMessage(IntegrationBaseMessage message, string topicName)
+        {
+            ISenderClient topicClient = new TopicClient(connectionString, topicName);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            var serviceBusMessage = new Message(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+            await topicClient.SendAsync(serviceBusMessage);
+            Console.WriteLine($"Sent message to {topicClient.Path}");
+            await topicClient.CloseAsync();
+
+        }
+    }
+}
